635bbc8f9355bb056dc339fdd2677f445cfe6599
Your Name <you@example.com>
2025-03-11 09:18:18 +0800

    init shell program code and use git interpreter

---
+++ bash/main.sh
@@ -0,0 +1,13 @@
+#!"C:\Program Files\Git\bin\bash"
+# 设置编码为UTF-8
+export LANG=en_US.UTF-8
+
+# 打印当前日期和时间
+echo "当前日期和时间: $(date)"
+
+# 打印当前工作目录
+echo "当前工作目录: $(pwd)"
+
+# 列出当前目录下的所有文件和目录
+echo "当前目录下的所有文件和目录:"
+ls -la

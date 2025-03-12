c00b467ce2fbd7016ff11cb57232a0d81b186d90
Your Name <you@example.com>
2025-03-12 14:12:11 +0800

    init batch program code

---
+++ batch/main.bat
@@ -0,0 +1,21 @@
+
+@echo off
+REM 关闭命令回显
+
+echo Hello, World!
+REM 输出 "Hello, World!" 到控制台
+
+set MY_VAR=123
+REM 设置环境变量 MY_VAR 的值为 123
+
+echo The value of MY_VAR is %MY_VAR%
+REM 输出环境变量 MY_VAR 的值
+
+::pause
+REM 暂停脚本执行，等待用户按键
+
+dir
+REM 列出当前目录下的文件和文件夹 
+
+REM 结束脚本
+exit
